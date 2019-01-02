7efcf66fbef7c033b0ceb4b932c530fcd0081f71
Daniel Barbero <dbarbero@viewnext.com>
2019-01-02 11:27:27 +0100

    img shopping list

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,6 +2,6 @@ export const APPCONFIG = {
   // Daniel 1 - Dolores 2
     user: '1',
     reloadList: true,
-    //API: "http://recetas.danielbarbero.es/api/public/index.php/api/v1/"
-    API: "http://localhost/dummy/public/index.php/api/v1/"
+    API: "http://recetas.danielbarbero.es/api/public/index.php/api/v1/"
+    //API: "http://localhost/dummy/public/index.php/api/v1/"
 }
